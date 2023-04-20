b146ac431442d26c08787a0cc8bb98afd8debf9b
Admin <Admin@10.100.99.21>
2023-04-20 20:54:14 +0200

    commit

--- TEMA8/src/ejercicio03/MainProductos.java
+++ TEMA8/src/ejercicio03/MainProductos.java
@@ -1,15 +1,47 @@
 package ejercicio03;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class MainProductos {
+	
+	static Scanner sc = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		Productos producto = new Productos(0);
+		Productos[] producto = new Productos[0];
 		
 		
 	}
 	
 	public static void menu() {
-		System.out.println("");
+		System.out.println("Introduzca una opción: "
+				+ "\n1. Mostrar productos."
+				+ "\n2. Crear un nuevo producto."
+				+ "\n3. Salir.");
+	}
+	
+	public static Productos[] crearProd(Productos[] producto, int contador){
+		
+		producto = Arrays.copyOf(producto, producto.length);
+		
+		System.out.println("Introduzca el nombre del producto: ");
+		producto[contador].setNombre(sc.nextLine());
+		
+		
+		return producto;
+	}
+	
+	public static int elijeTipo() {
+		int eleccion;
+		
+		do{
+			System.out.println("Elija un tipo de producto: "
+				+ "\n1. Perecedero."
+				+ "\n2. No perecedero.");
+			eleccion = sc.nextInt();
+		}while(eleccion!=1 && eleccion!=2);
+		
+		return eleccion;
 	}
 
 }
