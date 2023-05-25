614380d55dfa1d8caa4cc327ef74eba7271350d4
frodriguez <frodriguez@106-24.info2>
2023-05-25 11:26:13 +0200

    dejame

---
+++ TEMA8/src/ejercicio03/prueba.java
@@ -0,0 +1,12 @@
+package ejercicio03;
+
+public class prueba extends Perecederos{
+	int golazo = 0;
+
+	public prueba(String nombre, double precio, int diasACaducar, int golazo) {
+		super(nombre, precio, diasACaducar);
+		this.golazo = golazo;
+	}
+	
+	
+}
--- TEMA8_Interface/src/ejercicio04/Animal.java
+++
@@ -1,9 +0,0 @@
-package ejercicio04;
-
-public interface Animal {
-	public void comer();
-
-	public void dormir();
-
-	public void hacerRuido();
-}
